bbb0e2fadfcf3e8c1f7f882b5667713bb3f3d973
Anshuman202 <asanshuman2@gmail.com>
2023-01-13 23:26:03 +0530

    method calling

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace HashTableAssignment
+{
+    namespace HashTable
+    {
+        class Program
+        {
+            public static void Main(string[] args)
+            {
+                Linked_Hash_Map<string, int> linked_hash_map = new Linked_Hash_Map<string, int>(5);
+
+                string Sentence = "to be or not to be";
+                string[] Words = Sentence.Split(" ");
+                foreach (string word in Words)
+                {
+                    int Value = linked_hash_map.Get(word);
+                    if (Value == default)
+                        Value = 1;
+                    else Value++;
+                    linked_hash_map.Add(word, Value);
+                }
+                int Frequency = linked_hash_map.Get("to");
+                Console.WriteLine("Frquency of word in mentioned statement is : " + Frequency);
+            }
+        }
+    }
+
+}
